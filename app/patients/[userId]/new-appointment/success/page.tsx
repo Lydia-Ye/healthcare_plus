62964b6172d2d1cac6431657dc188ede7f0216c7
Lydia(Zi) Ye <ziyelydia@gmail.com>
2024-11-25 20:57:03 -0600

    implemented functionality to get dynamic appointment info

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -2,8 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { formatDateTime } from "@/lib/utils";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { Doctors } from "@/constants";
 
-const Success = async ({ params: { userId } }: SearchParamProps) => {
+const Success = async ({
+  searchParams,
+  params: { userId },
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician,
+  );
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
@@ -33,7 +46,25 @@ const Success = async ({ params: { userId } }: SearchParamProps) => {
 
         <section className="request-details">
           <p>Requested appointment details: </p>
-          <div className="flex items-center gap-3"></div>
+          <div className="flex items-center gap-3">
+            <Image
+              src={doctor?.image!}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-6"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              height={24}
+              width={24}
+              alt="calendar"
+            />
+            <p> {formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
         </section>
 
         <Button variant="outline" className="shad-primary-btn" asChild>
